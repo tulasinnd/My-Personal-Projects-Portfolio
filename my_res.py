1d942d40fb90819d0e6c6e2bab231ec692852889
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-09 01:12:59 +0530

    Update my_res.py

--- my_res.py
+++ my_res.py
@@ -1,17 +1,10 @@
 import streamlit as st
 
-st.markdown("""
-    <style>
-        .big-font {
-            font-size: 48px !important;
-        }
-        .title {
-            background: linear-gradient(to right, #f6d365, #fda085);
-            -webkit-background-clip: text;
-            -webkit-text-fill-color: transparent;
-            text-align: center;
-        }
-    </style>
-""", unsafe_allow_html=True)
+# Set page title
+st.set_page_config(page_title="My Portfolio")
 
-st.markdown('<h1 class="big-font title">My Portfolio</h1>', unsafe_allow_html=True)
+# Add title with pink color
+st.write(
+    f'<h1 style="color:#ff69b4;">My Portfolio</h1>',
+    unsafe_allow_html=True
+)
