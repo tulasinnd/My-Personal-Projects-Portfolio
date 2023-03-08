a67cefbc07d847f66a69bdcc8e2af77ef5268fb1
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-09 01:18:54 +0530

    Update my_res.py

--- my_res.py
+++ my_res.py
@@ -1,10 +1,31 @@
 import streamlit as st
 
-# Set page title
-st.set_page_config(page_title="MY PORTFOLIO")
-
-# Add title with pink color
-st.write(
+# Create a function to display the homepage
+def homepage():
+    st.title("Welcome to my portfolio")
+    st.write(
     f'<h1 style="color:#ff69b4;">MY PORTFOLIO</h1>',
     unsafe_allow_html=True
 )
+
+# Create a function to display the about page
+def about():
+    st.title("About Me")
+
+# Create a function to display the contact page
+def contact():
+    st.title("Contact Me")
+
+# Define the pages dictionary
+pages = {
+    "Homepage": homepage,
+    "About": about,
+    "Contact": contact
+}
+
+# Add a navigation menu to the sidebar
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# Call the appropriate page based on the user's menu choice
+pages[selection]()
+
