5b792901f0fbd9415297090028f1aff752cc46f3
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-09 01:23:46 +0530

    Update my_res.py

--- my_res.py
+++ my_res.py
@@ -1,49 +1,33 @@
-# import streamlit as st
-# st.set_page_config(layout="wide")
-
-# # Create a function to display the homepage
-# def homepage():
-#     st.title("Welcome to my portfolio")
-#     st.write(
-#     f'<h1 style="color:#ff69b4;">MY PORTFOLIO</h1>',
-#     unsafe_allow_html=True
-# )
-
-# # Create a function to display the about page
-# def about():
-#     st.title("About Me")
-
-# # Create a function to display the contact page
-# def contact():
-#     st.title("Contact Me")
-
-# # Define the pages dictionary
-# pages = {
-#     "Homepage": homepage,
-#     "About": about,
-#     "Contact": contact
-# }
-
-# # Add a navigation menu to the sidebar
-# selection = st.sidebar.radio("Go to", list(pages.keys()))
-
-# # Call the appropriate page based on the user's menu choice
-# pages[selection]()
-
-
 import streamlit as st
+st.set_page_config(layout="wide")
 
-# Create a container for the navigation menu
-nav_container = st.container()
+# Create a function to display the homepage
+def homepage():
+    st.title("Welcome to my portfolio")
+    st.write(
+    f'<h1 style="color:#ff69b4;">MY PORTFOLIO</h1>',
+    unsafe_allow_html=True
+)
 
-# Add items to the navigation menu
-nav_container.write("Home")
-nav_container.write("About")
-nav_container.write("Contact")
+# Create a function to display the about page
+def about():
+    st.title("About Me")
 
-# Create a separate container for the Streamlit content
-content_container = st.beta_container()
+# Create a function to display the contact page
+def contact():
+    st.title("Contact Me")
 
-# Add Streamlit content below the navigation menu
-content_container.write("Welcome to my Streamlit app!")
-content_container.write("This is the content area.")
+# Define the pages dictionary
+pages = {
+    "Homepage": homepage,
+    "About": about,
+    "Contact": contact
+}
+
+# Add a navigation menu to the sidebar
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# Call the appropriate page based on the user's menu choice
+pages[selection]()
+
+
