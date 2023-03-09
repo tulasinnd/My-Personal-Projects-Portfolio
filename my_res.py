d887791358f48af5e14d5e564c8eb8945d5007e5
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-03-09 13:13:39 +0530

    Update my_res.py

--- my_res.py
+++ my_res.py
@@ -12,6 +12,24 @@ def Overview():
 # Create a function to display the contact page
 def Contact():
     st.write( f'<h1 style="color:#ff69b4;">CONTACT INFORMATION</h1>', unsafe_allow_html=True )
+    pink_style = """
+            <style>
+                p {
+                    color: pink;
+                }
+            </style>
+        """
+
+        st.write(pink_style, unsafe_allow_html=True)
+        st.write("""
+            Here are some additional contact information you can consider including in your resume:
+
+            6. Personal Website or Portfolio: If you have a personal website or portfolio showcasing your work, include the URL in your contact information.
+            7. Social Media Handles: If you have professional social media accounts, such as Twitter or Instagram, you can include them in your contact information section. However, make sure that your profiles are appropriate for potential employers to view.
+            8. Skype or Zoom: If you're open to conducting initial interviews via video conferencing, you can include your Skype or Zoom username.
+            9. Availability: If you're currently employed or have other commitments, you can include your availability for interviews in your contact information section.
+            10. Professional Headline: You can include a professional headline or tagline underneath your name to quickly summarize your experience or career objective.
+        """)
     
 def Education():
     st.title("Education")
